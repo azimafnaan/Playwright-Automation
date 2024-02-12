caeb2c8cbbea0488e22e1050eb3efbddd78e85ad
Azim Afnaan <azim.afnaan@hotmail.com>
2024-02-13 01:17:45 +0600

    Installation and run first test case

---
+++ tests/HomePageTest.spec.js
@@ -0,0 +1,13 @@
+const { test, expect } = require('@playwright/test');
+
+test('Home Page', async ({ page }) => {
+    await page.goto("https://www.demoblaze.com/");
+    const pageTitle = await page.title();
+    console.log("Page Title is:", pageTitle);
+    await expect(page).toHaveTitle('STORE');
+    const pageUrl = await page.url();
+    console.log("Page URL is:", pageUrl);
+    await expect(page).toHaveURL('https://www.demoblaze.com/');
+    await page.close();
+
+})
